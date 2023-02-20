7c776aaa36c9d3bf21dca83a55be521d2e2e30c4
KirschX <mk44879@gmail.com>
2023-02-20 20:54:54 +0900

    fix: fixed layout, adjusted msw

--- front/src/mocks/handlers.js
+++ front/src/mocks/handlers.js
@@ -2,7 +2,8 @@ import { rest } from "msw";
 
 export const handlers = [
 
-  rest.get("https://msw.com/api/posts", async (req, res, ctx) => {
+  rest.get("https://msw.com/api/board", async (req, res, ctx) => {
+  
     return res(
       ctx.json(
           [  {
@@ -39,42 +40,20 @@ export const handlers = [
     );
   }),
 
-  rest.get("https://msw.com/api/posts/1", async (req, res, ctx) => {
+  rest.get("https://msw.com/api/board/:id", async (req, res, ctx) => {
+    const { id : post_id } = req.params
+    console.log(post_id)
     return res(
       ctx.json({
-        results: [
-            {
-              title: 'title1',
-              post_id: 1,
-              content: '안녕하세요, 알고리즘 질문드립니다',
-              created_date: '2012-10-12',
-              modified_date: '2012-10-12',
-              view_count: '12',
-              user: { id: 'id- 1', level: '1', problem_count: '10' },
-              comment: [{ id: 1 }, { id: 1 }, { id: 1 }],
-            },
-            {
-              title: 'title2',
-              post_id: 2,
-              content: '안녕하세요, 알고리즘 질문드립니다',
-              created_date: '2012-10-12',
-              modified_date: '2012-10-12',
-              view_count: '12',
-              user: { id: 'id- 1', level: '1', problem_count: '10' },
-              comment: [{ id: 1 }, { id: 1 }, { id: 1 }],
-            },
-            {
-              title: 'title3',
-              post_id: 3,
-              content: '안녕하세요, 알고리즘 질문드립니다',
-              created_date: '2012-10-12',
-              modified_date: '2012-10-12',
-              view_count: '12',
-              user: { id: 'id- 1', level: '1', problem_count: '10' },
-              comment: [{ id: 1 }, { id: 1 }, { id: 1 }],
-            },
-          ],
-      })
+            title: 'title1',
+            post_id: post_id,
+            content: '안녕하세요, 알고리즘 질문드립니다 알고리즘 질문드립니다.알고리즘 질문드립니다.알고리즘 질문드립니다.알고리즘 질문드립니다.알고리즘 질문드립니다.',
+            created_date: '2012-10-12',
+            modified_date: '2012-10-12',
+            view_count: '12',
+            user: { id: 'id- 1', level: '1', problem_count: '10' },
+            comment: [{ id: 1, content : "알고리즘 질문드립니다.", user : { name : "익명"}, created_date : "2022-12-12" }, { id: 2, content : "댓글내용2 입니다", user : { name : "익명"}, created_date : "2022-12-12" }, { id: 3, content : "댓글내용3 입니다", user : { name : "익명"}, created_date : "2022-12-12" }],
+          })
     );
   }),
 ];
--- front/src/pages/Board/BoardDetail.jsx
+++ front/src/pages/Board/BoardDetail.jsx
@@ -1,61 +1,22 @@
+import useSWR from 'swr';
+import { useParams } from 'react-router-dom';
+
 export default function BoardDetail() {
-  const post_data = [
-    {
-      title: 'title1',
-      post_id: 1,
-      content: '안녕하세요, 알고리즘 질문드립니다',
-      created_date: '2012-10-12',
-      modified_date: '2012-10-12',
-      view_count: '12',
-      user: { id: 'id- 1', level: '1', problem_count: '10' },
-      comment: [{ id: 1 }, { id: 1 }, { id: 1 }],
-    },
-    {
-      title: 'title2',
-      post_id: 2,
-      content: '안녕하세요, 알고리즘 질문드립니다',
-      created_date: '2012-10-12',
-      modified_date: '2012-10-12',
-      view_count: '12',
-      user: { id: 'id- 1', level: '1', problem_count: '10' },
-      comment: [{ id: 1 }, { id: 1 }, { id: 1 }],
-    },
-    {
-      title: 'title3',
-      post_id: 3,
-      content: '안녕하세요, 알고리즘 질문드립니다',
-      created_date: '2012-10-12',
-      modified_date: '2012-10-12',
-      view_count: '12',
-      user: { id: 'id- 1', level: '1', problem_count: '10' },
-      comment: [{ id: 1 }, { id: 1 }, { id: 1 }],
-    },
-  ];
+  let { id } = useParams();
+  const { data, error, isLoading } = useSWR(`https://msw.com/api/board/${id}`);
 
   return (
-    <div>
-      <span className="inline-flex my-3 ml-4 items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-800">
-        질문
-      </span>
-      {/* <div className="flex mb-3 px-4 cursor-pointer pb-3  border-b items-center space-x-3">
-        <div className="w-10 h-10 rounded-full bg-slate-300" />
-        <div>
-          <p className="text-sm font-medium text-gray-700">Steve Jebs</p>
-          <p className="text-xs font-medium text-gray-500">
-            View profile &rarr;
-          </p>
-        </div>
-      </div> */}
+    <div className=" pt-2">
       <div>
         <div className="mt-2 px-4 text-gray-700">
-          <span className="text-orange-500 font-medium">Q.</span> 알고리즘
-          질문드립니다.
+          <span className=" inline-flex my-3 items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-800">
+            질문
+          </span>
+
+          <span className=" font-bold ml-2">{data?.title}</span>
         </div>
         <div className="mt-2 px-4 text-gray-700 text-sm min-h-[100px]">
-          알고리즘 질문드립니다. 알고리즘 문제 내용은 ~~입니다. 알고리즘
-          질문드립니다. 알고리즘 문제 내용은 ~~입니다. 알고리즘 질문드립니다.
-          알고리즘 문제 내용은 ~~입니다. 알고리즘 질문드립니다. 알고리즘 문제
-          내용은 ~~입니다.
+          {data?.content}
         </div>
         <div className="flex px-4 space-x-5 mt-3 text-gray-700 py-2.5 border-t border-b-[2px]  w-full">
           <span className="flex space-x-2 items-center text-sm">
@@ -73,25 +34,27 @@ export default function BoardDetail() {
                 d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
               ></path>
             </svg>
-            <span>코멘트 1</span>
+            <span>코멘트 {data?.comment?.length}</span>
           </span>
         </div>
       </div>
       <div className="px-4 my-5 space-y-5">
-        <div className="flex items-start space-x-3">
-          <div className="w-8 h-8 bg-slate-200 rounded-full" />
-          <div className=" w-full">
-            <div className=" flex justify-between">
-              <span className="text-sm block font-medium text-gray-700">
-                Steve Jebs
-              </span>
-              <span className="text-xs text-gray-500 block ">2시간 전</span>
+        {data?.comment.map((item, i) => (
+          <div className="flex items-start space-x-3">
+            <div className="w-8 h-8 bg-slate-200 rounded-full" />
+            <div className=" w-full">
+              <div className=" flex justify-between">
+                <span className="text-sm block font-medium text-gray-700">
+                  {data?.user?.id}
+                </span>
+                <span className="text-xs text-gray-500 block ">
+                  {item?.created_date}
+                </span>
+              </div>
+              <p className="text-gray-700 mt-2 text-sm">{item?.content}</p>
             </div>
-            <p className="text-gray-700 mt-2 text-sm">
-              The best mandu restaurant is the one next to my house.
-            </p>
           </div>
-        </div>
+        ))}
       </div>
       <div className="px-4">
         <textarea
