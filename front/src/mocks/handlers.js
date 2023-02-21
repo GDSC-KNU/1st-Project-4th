44824867f9406121c4679d4e2161a41534fccec2
KirschX <mk44879@gmail.com>
2023-02-22 00:34:15 +0900

    chore

--- front/src/mocks/handlers.js
+++ front/src/mocks/handlers.js
@@ -31,7 +31,7 @@ const posts = [  {
   comment: [{ id: 1 }, { id: 1 }, { id: 1 }],
 }]
 
-const comments = [{ id: 1, content : "알고리즘 질문드립니다.", user : { name : "익명"}, created_date : "2022-12-12" }, { id: 2, content : "댓글내용2 입니다", user : { name : "익명"}, created_date : "2022-12-12" }, { id: 3, content : "댓글내용3 입니다", user : { name : "익명"}, created_date : "2022-12-12" }]
+const comments = [{ id: 1, content : "알고리즘 질문드립니다.", user : { id: 1 , name : "익명"}, created_date : "2022-12-12" }, { id: 2, content : "댓글내용2 입니다", user : { id: 2 , name : "익명"}, created_date : "2022-12-12" }, { id: 3, content : "댓글내용3 입니다", user : {  id: 3 , name : "익명"}, created_date : "2022-12-12" }]
 
 export const handlers = [
 
@@ -74,7 +74,7 @@ export const handlers = [
 
   rest.post("https://msw.com/api/comment", (req, res, ctx) => {
     let {comment} = req.body
-    let new_comment = { id: comments.length + 1, content : comment, user : { name : "익명"}}
+    let new_comment = { id: comments.length + 1, content : comment, user : { id: comments.length + 1, name : "익명"}}
     comments.push(new_comment)
     return res(ctx.json(new_comment));
   })
