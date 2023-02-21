f1891d3b9fe96de7cc808491a026f3143ea655ff
KirschX <mk44879@gmail.com>
2023-02-21 17:54:39 +0900

    feat: added Input(comment) component with useMutation

--- front/src/mocks/handlers.js
+++ front/src/mocks/handlers.js
@@ -1,48 +1,56 @@
 import { rest } from "msw";
 
+const posts = [  {
+  title: 'title1',
+  post_id: 1,
+  content: '안녕하세요, 알고리즘 질문드립니다',
+  created_date: '2012-10-12',
+  modified_date: '2012-10-12',
+  view_count: '12',
+  user: { id: 'id- 1', level: '1', problem_count: '10' },
+  comment: [{ id: 1 }, { id: 1 }, { id: 1 }],
+},
+{
+  title: 'title2',
+  post_id: 2,
+  content: '안녕하세요, 알고리즘 질문드립니다',
+  created_date: '2012-10-12',
+  modified_date: '2012-10-12',
+  view_count: '12',
+  user: { id: 'id- 1', level: '1', problem_count: '10' },
+  comment: [{ id: 1 }, { id: 1 }, { id: 1 }],
+},
+{
+  title: 'title3',
+  post_id: 3,
+  content: '안녕하세요, 알고리즘 질문드립니다',
+  created_date: '2012-10-12',
+  modified_date: '2012-10-12',
+  view_count: '12',
+  user: { id: 'id- 1', level: '1', problem_count: '10' },
+  comment: [{ id: 1 }, { id: 1 }, { id: 1 }],
+}]
+
+const comments = [{ id: 1, content : "알고리즘 질문드립니다.", user : { name : "익명"}, created_date : "2022-12-12" }, { id: 2, content : "댓글내용2 입니다", user : { name : "익명"}, created_date : "2022-12-12" }, { id: 3, content : "댓글내용3 입니다", user : { name : "익명"}, created_date : "2022-12-12" }]
+
 export const handlers = [
 
+  //board
   rest.get("https://msw.com/api/board", async (req, res, ctx) => {
-  
     return res(
-      ctx.json(
-          [  {
-            title: 'title1',
-            post_id: 1,
-            content: '안녕하세요, 알고리즘 질문드립니다',
-            created_date: '2012-10-12',
-            modified_date: '2012-10-12',
-            view_count: '12',
-            user: { id: 'id- 1', level: '1', problem_count: '10' },
-            comment: [{ id: 1 }, { id: 1 }, { id: 1 }],
-          },
-          {
-            title: 'title2',
-            post_id: 2,
-            content: '안녕하세요, 알고리즘 질문드립니다',
-            created_date: '2012-10-12',
-            modified_date: '2012-10-12',
-            view_count: '12',
-            user: { id: 'id- 1', level: '1', problem_count: '10' },
-            comment: [{ id: 1 }, { id: 1 }, { id: 1 }],
-          },
-          {
-            title: 'title3',
-            post_id: 3,
-            content: '안녕하세요, 알고리즘 질문드립니다',
-            created_date: '2012-10-12',
-            modified_date: '2012-10-12',
-            view_count: '12',
-            user: { id: 'id- 1', level: '1', problem_count: '10' },
-            comment: [{ id: 1 }, { id: 1 }, { id: 1 }],
-          }]
-      )
+      ctx.json(posts)
     );
   }),
 
+  rest.post("https://msw.com/api/board", (req, res, ctx) => {
+    posts.push(req.body);
+    return res(ctx.status(201));
+  }),
+
+  //borad/[id]
   rest.get("https://msw.com/api/board/:id", async (req, res, ctx) => {
     const { id : post_id } = req.params
-    console.log(post_id)
+
     return res(
       ctx.json({
             title: 'title1',
@@ -52,8 +60,23 @@ export const handlers = [
             modified_date: '2012-10-12',
             view_count: '12',
             user: { id: 'id- 1', level: '1', problem_count: '10' },
-            comment: [{ id: 1, content : "알고리즘 질문드립니다.", user : { name : "익명"}, created_date : "2022-12-12" }, { id: 2, content : "댓글내용2 입니다", user : { name : "익명"}, created_date : "2022-12-12" }, { id: 3, content : "댓글내용3 입니다", user : { name : "익명"}, created_date : "2022-12-12" }],
+            comment: comments,
           })
     );
   }),
+
+  // comment
+  rest.get("https://msw.com/api/comment", async (req, res, ctx) => {
+    return res(
+      ctx.json(comments)
+    );
+  }),
+
+  rest.post("https://msw.com/api/comment", (req, res, ctx) => {
+    let {comment} = req.body
+    let new_comment = { id: comments.length + 1, content : comment, user : { name : "익명"}}
+    comments.push(new_comment)
+    return res(ctx.status(201));
+  })
+
 ];
