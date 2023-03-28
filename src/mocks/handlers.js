37c6aa5aea5e439431b194e4866efff436b7932e
KirschX <mk44879@gmail.com>
2023-03-28 19:40:52 +0900

    fix(view): board list page layout fixed

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,67 +1,84 @@
 import { rest } from "msw";
 
-const posts = [  {
-  title: 'title1',
-  post_id: 1,
-  content: '안녕하세요, 알고리즘 질문드립니다',
-  created_date: '2012-10-12',
-  modified_date: '2012-10-12',
-  view_count: '12',
-  user: { id: 'id- 1', level: '1', problem_count: '10' },
-  comment: [{ id: 1 }, { id: 1 }, { id: 1 }],
-},
-{
-  title: 'title2',
-  post_id: 2,
-  content: '안녕하세요, 알고리즘 질문드립니다',
-  created_date: '2012-10-12',
-  modified_date: '2012-10-12',
-  view_count: '12',
-  user: { id: 'id- 1', level: '1', problem_count: '10' },
-  comment: [{ id: 1 }, { id: 1 }, { id: 1 }],
-},
-{
-  title: 'title3',
-  post_id: 3,
-  content: '안녕하세요, 알고리즘 질문드립니다',
-  created_date: '2012-10-12',
-  modified_date: '2012-10-12',
-  view_count: '12',
-  user: { id: 'id- 1', level: '1', problem_count: '10' },
-  comment: [{ id: 1 }, { id: 1 }, { id: 1 }],
-}]
+const posts_response = {
+  "success": true,
+  "code": 0,
+  "msg": "성공하였습니다.",
+  "list" : [{
+    title: 'title1',
+    id: 1,
+    description: '안녕하세요, 알고리즘 질문드립니다',
+    created_date: '2012-10-12',
+    modified_date: '2012-10-12',
+    view_count: '12',
+    user: { id: 'id- 1', level: '1', problem_count: '10' },
+    comment_count: 3,
+    status: null
+  },
+  {
+    title: 'title2',
+    id: 2,
+    description: '안녕하세요, 알고리즘 질문드립니다',
+    created_date: '2012-10-12',
+    modified_date: '2012-10-12',
+    view_count: '12',
+    user: { id: 'id- 1', level: '1', problem_count: '10' },
+    comment_count: 3,
+    status: null
+  },
+  {
+    title: 'title2',
+    id: 3,
+    description: '안녕하세요, 알고리즘 질문드립니다',
+    created_date: '2012-10-12',
+    modified_date: '2012-10-12',
+    view_count: '12',
+    user: { id: 'id- 1', level: '1', problem_count: '10' },
+    comment_count: 3,
+    status: null
+  }]
+}
 
 const comments = [{ id: 1, content : "알고리즘 질문드립니다.", user : { id: 1 , name : "익명"}, created_date : "2022-12-12" }, { id: 2, content : "댓글내용2 입니다", user : { id: 2 , name : "익명"}, created_date : "2022-12-12" }, { id: 3, content : "댓글내용3 입니다", user : {  id: 3 , name : "익명"}, created_date : "2022-12-12" }]
+
+
 
 export const handlers = [
 
   //board
   rest.get("https://msw.com/api/board", async (req, res, ctx) => {
     return res(
-      ctx.json(posts)
+      ctx.json(posts_response)
     );
   }),
 
   rest.post("https://msw.com/api/board", (req, res, ctx) => {
-    posts.push(req.body);
+    posts_response.push(req.body);
     return res(ctx.status(201));
   }),
 
   //borad/[id]
   rest.get("https://msw.com/api/board/:id", async (req, res, ctx) => {
-    const { id : post_id } = req.params
+    const { id } = req.params
 
     return res(
-      ctx.json({
+      ctx.json(
+        {
+          "success": true,
+          "code": 0,
+          "msg": "성공하였습니다.",
+          "data": {
             title: 'title1',
-            post_id: post_id,
-            content: '안녕하세요, 알고리즘 질문드립니다 알고리즘 질문드립니다.알고리즘 질문드립니다.알고리즘 질문드립니다.알고리즘 질문드립니다.알고리즘 질문드립니다.',
+            id: id,
+            description: '안녕하세요, 알고리즘 질문드립니다 알고리즘 질문드립니다.알고리즘 질문드립니다.알고리즘 질문드립니다.알고리즘 질문드립니다.알고리즘 질문드립니다.',
             created_date: '2012-10-12',
             modified_date: '2012-10-12',
             view_count: '12',
             user: { id: 'id- 1', level: '1', problem_count: '10' },
-            comment: comments,
-          })
+            commentList: comments,
+          }
+        }
+      )
     );
   }),
 
