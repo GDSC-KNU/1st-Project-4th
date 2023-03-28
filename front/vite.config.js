5696438cba1ac2c577948e8d5ff28ed39cee92b9
KirschX <mk44879@gmail.com>
2023-03-28 21:04:19 +0900

    chore

--- front/vite.config.js
+++ front/vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/1st-Project-4th/",
   plugins: [react()],
 })
+// https://gdsc-knu.github.io/1st-Project-4th/
