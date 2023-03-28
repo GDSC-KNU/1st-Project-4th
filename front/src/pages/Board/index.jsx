5c3823f1384b38b2b3ed9cb16e671d69095f815f
KirschX <mk44879@gmail.com>
2023-03-28 19:40:52 +0900

    fix(view): board list page layout fixed

--- front/src/pages/Board/index.jsx
+++ front/src/pages/Board/index.jsx
@@ -5,7 +5,7 @@ import { useState } from 'react';
 
 export default function Board() {
   const {
-    data: post_data,
+    data: { list: posts } = {},
     error,
     isLoading,
   } = useSWR('https://msw.com/api/board');
@@ -15,48 +15,53 @@ export default function Board() {
     setCurrentPage(pageNumber);
   };
 
+  // console.log(posts);
+
   return (
-    <div className="  min-w-[300px] w-full pt-6 ">
-      <div className=" space-y-2 px-3 ">
-        {post_data &&
-          post_data.map(
+    <div className="  min-w-[300px] w-full pt-6 px-3">
+      <div className=" flex">
+        <div className=" p-2 border-[1px] border-solid cursor-pointer">
+          질문 게시판
+        </div>
+        <div className=" p-2 border-[1px] border-solid cursor-pointer">
+          자유 게시판
+        </div>
+      </div>
+      <div className=" border-[1px] mb-4 relative bottom-[1px]"></div>
+      <div className=" space-y-2 mb-8">
+        {posts &&
+          posts.map(
             (
               {
                 title,
-                content,
+                description,
                 created_date,
                 modified_date,
                 view_count,
-                comment,
-                post_id,
+                comment_count,
+                id,
               },
               i,
             ) => (
-              <div key={post_id} className=" flex flex-col items-start">
+              <div key={id} className=" flex flex-col items-start">
                 <span className=" mb-1 flex items-center px-2.5  rounded-full text-xs font-medium bg-gray-100 text-gray-800 ">
                   질문
                 </span>
                 <div className="  text-gray-700">
-                  <Link
-                    className=" cursor-pointer"
-                    key={post_id}
-                    to={`/board/${i}`}
-                  >
+                  <Link className=" cursor-pointer" key={id} to={`/board/${i}`}>
                     <span className=" text-blue-500 font-bold hover:underline">
                       {title}
                     </span>
                   </Link>
-                  <span className=" text-sm">
-                    {' ' + `[${comment.length}]`}
-                  </span>
+                  <span className=" text-sm">{' ' + `[${comment_count}]`}</span>
                 </div>
                 <div className="  text-gray-700 text-sm">
                   <span className=" "></span>
-                  {content.slice(0, 50) + '...'}
+                  {description.slice(0, 50) + '...'}
                 </div>
                 <div
                   className={
-                    i === post_data.length - 1
+                    posts && i === posts?.length - 1
                       ? ' flex items-center justify-between w-full text-gray-500 font-medium text-xs'
                       : ' flex items-center justify-between w-full text-gray-500 font-medium text-xs border-b-[1px] border-gray-200 pb-[2px]'
                   }
@@ -72,11 +77,30 @@ export default function Board() {
             ),
           )}
       </div>
+      <div className=" flex justify-between">
+        <div>
+          <ol className=" flex">
+            <li className=" border-[1px] border-solid p-2">전체글</li>
+            <li className=" border-[1px] border-solid p-2">인기글</li>
+            <li className="">
+              <select class="bg-gray-50 border border-gray-300 text-gray-900 text-sm h-full focus:ring-blue-500 focus:border-blue-500 block w-fit dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <option selected>정렬</option>
+                <option value="US">등록순</option>
+                <option value="CA">추천순</option>
+                <option value="FR">댓글갯수순</option>
+              </select>
+            </li>
+          </ol>
+        </div>
+        <div className=" border-[1px] border-solid p-2 cursor-pointer">
+          글쓰기
+        </div>
+      </div>
       <div className=" pt-10">
         <Pagination
           currentPage={currentpage}
           itemsCountPerpage={10}
-          totalItemsCount={122}
+          ro
           pageRangeDisplayed={5}
           onChange={onPageChangeHandler}
         />
