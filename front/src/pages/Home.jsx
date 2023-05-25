ccab027a6bee57bbe24e6d0f632902f9cbc12f5d
edmund <edmund1000@naver.com>
2023-05-25 21:49:07 +0900

    jjk

--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -5,36 +5,35 @@ export default function Home() {
     { id: 3, name: '알고리즘 문제3', level: 2 },
     { id: 4, name: '알고리즘 문제4', level: 3 },
     { id: 5, name: '알고리즘 문제5', level: 5 },
+    { id: 6, name: '알고리즘 문제6', level: 5 },
   ];
-  // const data = fetch('')
 
   return (
-    <div className=" w-full h-full">
-      <div className=" grid grid-rows-3 ">
-        <div className=" bg-gray-300 row-span-1 items-center flex justify-center text-4xl font-bold">
-          Welcome to AlgoHelper
+    <div className="w-full h-full">
+      <div className="grid grid-rows-3">
+        <div className="row-span-1 flex items-center justify-center">
+          <h1 className="text-4xl font-bold text-center">
+            Welcome to <span className="text-blue-600">AlgoHelper</span>
+          </h1>
         </div>
 
-        <div className="  row-span-2">
-          <div className=" bg-gray-200 h-full">
-            <div className="  h-full md:mx-20 mt-10 p-3">
-              <div className=" p-2 font-bold text-xl mt-6">
-                오늘의 문제 추천
-              </div>
-              <ul>
-                {temp.map((item, index) => (
-                  <li
-                    className=" w-full flex justify-between p-3 items-center"
-                    key={item.name}
+        <div className="row-span-2">
+          <div className="h-full">
+            <div className="h-full md:mx-20 mt-10 p-3">
+              <div className="mt-8 p-2 font-bold text-3xl text-center">오늘의 문제 추천</div>
+
+              <div className="mt-8 grid grid-cols-3 gap-4">
+                {temp.map((item) => (
+                  <div
+                    key={item.id}
+                    className="bg-white rounded-lg shadow-md p-8 mb-4 w-full flex flex-col items-center justify-center"
                   >
-                    {item.name}
-                    <span className=" flex text-sm text-gray-500">
-                      <p>유형: DFS</p>
-                      <p className=" ml-8">난이도 {item.level}</p>
-                    </span>
-                  </li>
+                    <h3 className="font-bold text-center">
+                      <span className="inline-block w-full">{item.name}</span> {/* w-full 클래스 추가 */}
+                    </h3>
+                  </div>
                 ))}
-              </ul>
+              </div>
             </div>
           </div>
         </div>
